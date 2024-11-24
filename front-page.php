9f6fd7fe4aa11d828e933e56353b59aa49fbe61f
Scot Newbury <scot@scotnewbury.com>
2024-11-24 15:55:17 -0500

    Updating cover image and text

--- front-page.php
+++ front-page.php
@@ -1,32 +1,32 @@
 <?php get_header(); ?>
-  <div id="banner">
-    <h1>St. Mark's Lodge #44</h1>
-  </div>
+<div id="banner">
+  <h1>St. Mark's Lodge No. 44</h1>
+</div>
 
-  <main>
-    <a href="<?php echo site_url('/news-events'); ?>">
-      <h2 class="section-heading">News &amp Events</h2>
-    </a>
+<main>
+  <a href="<?php echo site_url('/news-events'); ?>">
+    <h2 class="section-heading">News &amp Events</h2>
+  </a>
 
-    <section>
-      <?php
+  <section>
+    <?php
 
-        $args = array (
-          'post_type' => 'post',
-          'posts_per_page' => 2,
-        );
+    $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => 2,
+    );
 
-        $blogposts = new WP_Query($args);
+    $blogposts = new WP_Query($args);
 
-        while($blogposts -> have_posts()) {
-          $blogposts -> the_post();
-      ?>
-      
+    while ($blogposts->have_posts()) {
+      $blogposts->the_post();
+    ?>
+
       <div class="card border-0">
-        <?php if(has_post_thumbnail(get_the_ID())) { ?>
+        <?php if (has_post_thumbnail(get_the_ID())) { ?>
           <a href="<?php the_permalink(); ?>">
             <div class="card-image">
-              <img src="<?php echo get_the_post_thumbnail_url( get_the_ID()) ?>" alt="Card Image">
+              <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()) ?>" alt="Card Image">
             </div>
           </a>
         <?php } else { ?>
@@ -34,20 +34,20 @@
             <div class="card-image">
               <img src="<?php echo get_template_directory_uri(); ?>/img/logo-placeholder.png" alt="Lodge Logo">
             </div>
-          </a>        
+          </a>
         <?php } ?>
         <div class="card-description">
-          <a href="<?php the_permalink();?>">
-            <h3><?php the_title();?></h3>
+          <a href="<?php the_permalink(); ?>">
+            <h3><?php the_title(); ?></h3>
           </a>
-          <p><?php echo wp_trim_words(get_the_excerpt(), 30);?></p>
-          <a href="<?php the_permalink()?>" class="btn-readmore">Read More</a>
+          <p><?php echo wp_trim_words(get_the_excerpt(), 30); ?></p>
+          <a href="<?php the_permalink() ?>" class="btn-readmore">Read More</a>
         </div>
       </div>
-      <?php
-        }
-        wp_reset_query();
-      ?>
-    </section>
+    <?php
+    }
+    wp_reset_query();
+    ?>
+  </section>
 
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
